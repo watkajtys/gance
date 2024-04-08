23f085afa4d601a08da52fb911d94468c27a9ef2
watkajtys <watkajtys@gmail.com>
2024-04-08 00:35:04 -0600

    loading in marquee

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,18 +3,24 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 import Hero    from "~/components/gance/hero/hero";
 import Introduction from "~/components/gance/introduction/introduction";
+import Marquee from "~/components/gance/marquee/marquee";
 
 export default component$(() => {
   return (
     <>
       <Hero />
       <Introduction />
+      <div class={'container container-center'}>
+        <h2 class={'__center_text'}>Expanding Consciousness At Innovative Organizations</h2>
+        <Marquee />
+      </div>
+
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Jennifer Gance | Coaching for Leadership, Teams & Individuals",
+  title: "Jennnifer Gance Coaching & Consulting",
   meta: [
     {
       name: "description",
