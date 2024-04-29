b84fc536f6a832a070bfca9c7cec2d34bea1c4ab
watkajtys <watkajtys@gmail.com>
2024-04-29 02:59:41 -0600

    services update

--- src/routes/services/index.tsx
+++ src/routes/services/index.tsx
@@ -2,6 +2,8 @@ import {component$}   from "@builder.io/qwik";
 import type {DocumentHead} from "@builder.io/qwik-city";
 import styles from "~/components/gance/introduction/introduction.module.css";
 import Hero from "~/components/gance/hero/hero";
+import NewGanceImage from "../../media/newGanceImage.jpg?jsx";
+import Project from "../../media/project.jpg?jsx";
 
 export default component$(() => {
   return (
@@ -9,22 +11,35 @@ export default component$(() => {
         <Hero text={'Services'}/>
           <div class={["container", "container-white", styles.introduction]}>
             <div class={["container", "container-no-padding", styles.textWrap]}>
-              <div>
-                <h3 class={'margin'}>1:1 Coaching</h3>
-                <p>This is a service I provide for those looking for support in their own personal development.</p>
-                <p>Women and men of all ages exploring professional and or personal development (executive, leadership, career, spiritual, relationships, other). I support individuals in whatever their journey is calling for.</p>
-                <p><small>*All 1:1 engagements are offered a sliding scale. No one turned away for lack of funds.</small></p>
+              <h3 class={'margin'}>1:1 Coaching</h3>
+              <div class={["container", "container-no-padding", "container-flex"]}>
+                <NewGanceImage alt="jennifer gance" class={styles.headshot}/>
+                <div class={"flex-column"}>
+                  <p>This is a service I provide for those looking for support in their own personal development.</p>
+                  <p>Women and men of all ages exploring professional and or personal development (executive,
+                    leadership, career, spiritual, relationships, other). I support individuals in whatever their
+                    journey is calling for.</p>
+                  <p><small>*All 1:1 engagements are offered a sliding scale. No one turned away for lack of
+                    funds.</small></p>
+                </div>
               </div>
 
               <div>
                 <h3 class={'margin'}>Life Coaching & Spiritual Work</h3>
-                <p>We are not in control. When we try to control, we are creating an illusion of control. That is it. And when it doesn’t go our way, we suffer. Our attachments lead to controlling, and controlling leads to expectations. All ultimately lead to suffering.</p>
-                <p>I know that sounds harsh but it is true! There is a better and easier way to live though. It is truly living our lives from the inside out. </p>
+                <p>We are not in control. When we try to control, we are creating an illusion of control. That is it.
+                  And when it doesn’t go our way, we suffer. Our attachments lead to controlling, and controlling leads
+                  to expectations. All ultimately lead to suffering.</p>
+                <p>I know that sounds harsh but it is true! There is a better and easier way to live though. It is truly
+                  living our lives from the inside out. </p>
               </div>
 
               <div>
                 <h3 class={'margin'}>Executive & Leadership Coaching</h3>
-                <p>My 1:1 work is direct, effective and intense. It is only for people who are really ready. As a leader, you not only have a certain level of intellect, but possess exquisite emotional maturity. True leadership is rising to the occasion when times are rough, not just when things are going good. This takes a level of self-care and radical responsibility. Let’s work together to build your executive and leadership muscles for these times!
+                <p>My 1:1 work is direct, effective and intense. It is only for people who are really ready. As a
+                  leader, you not only have a certain level of intellect, but possess exquisite emotional maturity. True
+                  leadership is rising to the occasion when times are rough, not just when things are going good. This
+                  takes a level of self-care and radical responsibility. Let’s work together to build your executive and
+                  leadership muscles for these times!
                 </p>
               </div>
 
@@ -37,21 +52,27 @@ export default component$(() => {
                   <li><p>Interview preparation/mock interview</p></li>
                   <li><p>Burn out and well-being</p></li>
                 </ul>
-                <p>Getting right with who we are from the inside sets us up for “right” action that can feel effortless. Looking at our blind spots and closing our gaps will create a shift in your being, then the outer world absolutely HAS to shift! This is what some may call, “being in the flow.”</p>
+                <p>Getting right with who we are from the inside sets us up for “right” action that can feel effortless.
+                  Looking at our blind spots and closing our gaps will create a shift in your being, then the outer
+                  world absolutely HAS to shift! This is what some may call, “being in the flow.”</p>
               </div>
 
               <div>
                 <h3 class={'margin'}>Organizational Clientele & Industry Expertise</h3>
-                <p><b>Extended professional development engagements</b> - 3-months to 3-years+. All services are tailored the unique organizational needs:</p>
-                <ul>
-                  <li><p>Leadership and team coaching (1:1 and/or group)</p></li>
-                  <li><p>Corporate Training</p></li>
-                  <li><p>Tailored Learning and development</p></li>
-                  <li><p>Consulting</p></li>
-                  <li><p>And much more...</p></li>
-                </ul>
-
-                <ul>
+                <p><b>Extended professional development engagements</b> <br/>3-months to 3-years+. All services are
+                  tailored the unique organizational needs:</p>
+                <div class={['container', 'container-flex', 'container-no-padding']}>
+                  <ul>
+                    <li><p>Leadership and team coaching (1:1 and/or group)</p></li>
+                    <li><p>Corporate Training</p></li>
+                    <li><p>Tailored Learning and development</p></li>
+                    <li><p>Consulting</p></li>
+                    <li><p>And much more...</p></li>
+                  </ul>
+                  <Project alt="jennifer gance" class={styles.headshot}/>
+                </div>
+                <p><b>Industry Experience:</b></p>
+                <ul class={"two-columns"}>
                   <li><p>Healthcare</p></li>
                   <li><p>Creative Districts</p></li>
                   <li><p>Professional Showrooms for high-end design</p></li>
@@ -71,21 +92,25 @@ export default component$(() => {
               </div>
 
               <div>
-                <h3 class={'margin'}><b>Assessment Certifications</b></h3>
+                <h3 class={'margin'}>Assessment Certifications</h3>
+                <p><b><a href="mailto:jennifer@jennifergance.com">contact me</a> for other assessment offerings.</b></p>
                 <ul>
                   <li><p><b>Emotional Intelligence (EQ-i 2.0)</b> - Measures individual and 360 assessments.</p></li>
-                  <li><p><b>DRiV Assessment </b> - provides access and actionable approaches to what drives or drains behaviors.</p></li>
-                  <li><p><b>Energy Leadership Index (ELI)</b> - Individual and 360 assessments that measure attitudes in terms of energy.</p></li>
-                  <li><p><b>Enneagram</b> - The Creative Consciousness Foundation. Course study 2022-2023: ‘The Enneagram of Awakening.’</p></li>
-                  <li><p><b>DRiv assessment </b> - certification administered through OKA; identifies what drives and drains individuals.</p></li>
+                  <li><p><b>DRiV Assessment </b> - provides access and actionable approaches to what drives or drains
+                    behaviors.</p></li>
+                  <li><p><b>Energy Leadership Index (ELI)</b> - Individual and 360 assessments that measure attitudes in
+                    terms of energy.</p></li>
+                  <li><p><b>Enneagram</b> - The Creative Consciousness Foundation. Course study 2022-2023: ‘The
+                    Enneagram of Awakening.’</p></li>
+                  <li><p><b>DRiv assessment </b> - certification administered through OKA; identifies what drives and
+                    drains individuals.</p></li>
                 </ul>
 
               </div>
             </div>
 
             <div class={'spacer'}></div>
-            <a href="/public/Establishing%20Buy-In%20for%20Professional%20Development%20and%20Coaching.pdf" class={["cta-wide", "button"]} target={'_blank'}>Get your free guide to Establishing Buy-In with your Organization and Team for Professional Development!</a>
-
+            <a href="/establishing-buy-in.pdf" class={["cta-wide", "button"]} target={'_blank'}>Get your free guide to Establishing Buy-In with your Organization and Team for Professional Development!</a>
           </div>
 
       </>
