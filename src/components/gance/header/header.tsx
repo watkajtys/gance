da56b54b2f1ed5b35f418894b6193f8853829abb
watkajtys <watkajtys@gmail.com>
2024-04-29 03:13:49 -0600

    hiding elements on mobile

--- src/components/gance/header/header.tsx
+++ src/components/gance/header/header.tsx
@@ -9,7 +9,7 @@ export default component$(() => {
           <div class={styles.logo}>
             <a href="/" title="jennifer gance">
               <MainLogo/>
-              <div>
+              <div class={"__hide_on_mobile"}>
                 <h1>Jennifer Gance</h1>
                 <h2>Coaching & Consulting</h2>
               </div>
@@ -33,7 +33,7 @@ export default component$(() => {
                 Testimonials
               </a>
 
-              <a href="https://us4.list-manage.com/contact-form?u=c0f71ba84c0b47e885ab0cab1&form_id=9912b42e6cba6d343728cc4ced1b5e0d" class={styles.button}>
+              <a href="https://us4.list-manage.com/contact-form?u=c0f71ba84c0b47e885ab0cab1&form_id=9912b42e6cba6d343728cc4ced1b5e0d" class={[styles.button, "__hide_on_mobile"]}>
                 Free Consultation
               </a>
             </li>
